7a46ef9625846cf415f9b72f9928790d2068d095
shahriar2880 <shahriarreza436@gmail.com>
2025-05-15 02:26:32 +0600

    create a header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import Header from './components/header/Header'
 
 function App() {
 
   return (
-    <div className='text-center text-7xl'>
-      gg
+    <div className='container mx-auto'>
+      <Header/>
     </div>
   )
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">This is Header</h1>
+    </div>
+  )
+}
+
+export default Header
